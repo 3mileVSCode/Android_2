9b7bf546976e042684f84148ec9e0e20ac904ef2
3m3l3VSCode <laroucheemile87@gmail.com>
2026-08-27 16:07:14 -0400

    pour eloi

--- Annexe_1/app/src/main/java/com/decinfo/annexe_1/MainActivity.kt
+++ Annexe_1/app/src/main/java/com/decinfo/annexe_1/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.decinfo.annexe_1
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var boutonAjouter: Button
+    lateinit var boutonAfficher: Button
+    lateinit var boutonQuitter: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,53 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        boutonAfficher = findViewById(R.id.afficher)
+        boutonQuitter = findViewById(R.id.quitter)
+        boutonAjouter = findViewById(R.id.ajouter)
+
+        //1 etape
+        val ec = Ecouteur()
+
+        //2 etape
+        //boutonQuitter.setOnClickListener (ec)
+        boutonAjouter.setOnClickListener (ec)
+        boutonAfficher.setOnClickListener (ec)
+
+        // autre façon
+
+        // interface fonctionnelle : interface qui n'a qu'une méthode
+        // une interface fonctionnelle peut être remplacé par une expression lambda (arrow function)
+        // 
+
     }
+
+    //3e etape
+
+    inner class Ecouteur : View.OnClickListener
+    {
+        override fun onClick(v: View?) { //type est View ou null
+            if (v == boutonQuitter)
+                finish()
+            else if ( v == boutonAfficher)
+            {
+                val i = Intent( this@MainActivity, AfficherActivity::class.java)
+                startActivity(i)
+            }
+            else
+            {
+                val i = Intent( this@MainActivity, AjouterActivity::class.java)
+                startActivity(i)
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
 }
